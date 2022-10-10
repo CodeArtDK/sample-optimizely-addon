19902ca8a46ac70e4cb3d28e1e3077087f126352
Allan Thraen <allan@codeart.dk>
2022-10-10 21:14:40 +0200

    Create SampleOptimizelyAddOnController.cs

---
+++ src/SampleOptimizelyAddon/Controllers/SampleOptimizelyAddOnController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SampleOptimizelyAddon.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleOptimizelyAddon.Controllers
+{
+    public class SampleOptimizelyAddOnController : Controller
+    {
+        private SampleOptimizelyAddonOptions _options;
+        public SampleOptimizelyAddOnController(IOptions<SampleOptimizelyAddonOptions> options)
+        {
+            _options = options.Value;
+        }
+    }
+}
